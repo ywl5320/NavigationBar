ead0dd8db33b272bbf1aa2f4edbbb85d756ff0f1
yangwanli1990 <596353036@qq.com>
2016-07-17 15:17:04 +0800

    增加横向滚动菜单，适用于多菜单标题

--- NavigationBar/app/src/main/java/com/ywl5320/navigationbar/MainActivity.java
+++ NavigationBar/app/src/main/java/com/ywl5320/navigationbar/MainActivity.java
@@ -9,56 +9,72 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ywl5320.navigationbar.bar.NavitationLayout;
+import com.ywl5320.navigationbar.bar.NavitationScrollLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NavitationLayout navitationLayout;
+    private NavitationScrollLayout navitationScrollLayout;
     private ViewPager viewPager;
-    private String[] titles = new String[]{"标题一", "标题二", "标题三"};
+    private String[] titles = new String[]{"标题一", "标题二", "标题三", "标题四", "标题五", "标题六", "标题七","标题八"};
     private ViewPagerAdapter viewPagerAdapter;
     private List<Fragment> fragments;
+
+    private NavitationLayout navitationLayout;
+    private ViewPager viewPager1;
+    private String[] titles1 = new String[]{"标题一", "标题二", "标题三", "标题四"};
+    private ViewPagerAdapter viewPagerAdapter1;
+    private List<Fragment> fragments1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager1 = (ViewPager) findViewById(R.id.viewpager1);
 
-        navitationLayout = (NavitationLayout) findViewById(R.id.bar);
+        navitationScrollLayout = (NavitationScrollLayout) findViewById(R.id.bar);
+        navitationLayout = (NavitationLayout) findViewById(R.id.bar1);
+
+        fragments1 =  new ArrayList<>();
+        fragments1.add(new ChildFragment());
+        fragments1.add(new ChildFragment());
+        fragments1.add(new ChildFragment());
+        fragments1.add(new ChildFragment());
+        viewPagerAdapter1 = new ViewPagerAdapter(getSupportFragmentManager(), fragments1);
+        viewPager1.setAdapter(viewPagerAdapter1);
+
 
         fragments =  new ArrayList<>();
+        fragments.add(new ChildFragment());
+        fragments.add(new ChildFragment());
+        fragments.add(new ChildFragment());
+        fragments.add(new ChildFragment());
+        fragments.add(new ChildFragment());
         fragments.add(new ChildFragment());
         fragments.add(new ChildFragment());
         fragments.add(new ChildFragment());
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
         viewPager.setAdapter(viewPagerAdapter);
 
-//        navitationLayout.setViewPager(this, titles, viewPager, R.color.color_333333, R.color.color_2581ff, 16, 16, 0, 0, true);
-//        navitationLayout.setNavLine(this, 3, R.color.colorPrimary, 0);
-
-//        navitationLayout.setViewPager(this, titles, viewPager, R.color.color_333333, R.color.color_2581ff, 16, 16, 0, 0, true);
-//        navitationLayout.setBgLine(this, 1, R.color.colorAccent);
-//        navitationLayout.setNavLine(this, 3, R.color.colorPrimary, 0);
-//
-//        navitationLayout.setViewPager(this, titles, viewPager, R.color.color_333333, R.color.color_2581ff, 16, 16, 0, 12, true);
-//        navitationLayout.setBgLine(this, 1, R.color.colorAccent);
-//        navitationLayout.setNavLine(this, 3, R.color.colorPrimary, 0);
-
-        navitationLayout.setViewPager(this, titles, viewPager, R.color.color_333333, R.color.color_2581ff, 16, 16, 0, 12, true, R.color.color_333333, 1f, 15f, 15f);
+        navitationLayout.setViewPager(this, titles1, viewPager1, R.color.color_333333, R.color.color_2581ff, 16, 16, 0, 0, true);
         navitationLayout.setBgLine(this, 1, R.color.colorAccent);
         navitationLayout.setNavLine(this, 3, R.color.colorPrimary, 0);
 
-        navitationLayout.setOnTitleClickListener(new NavitationLayout.OnTitleClickListener() {
+        navitationScrollLayout.setViewPager(this, titles, viewPager, R.color.color_333333, R.color.color_2581ff, 16, 16, 12, true, R.color.color_333333, 0f, 15f, 15f, 100);
+        navitationScrollLayout.setBgLine(this, 1, R.color.colorAccent);
+        navitationScrollLayout.setNavLine(this, 3, R.color.colorPrimary);
+
+        navitationScrollLayout.setOnTitleClickListener(new NavitationScrollLayout.OnTitleClickListener() {
             @Override
             public void onTitleClick(View v) {
                 Toast.makeText(MainActivity.this, ((TextView)v).getText(), Toast.LENGTH_SHORT).show();
             }
         });
 
-        navitationLayout.setOnNaPageChangeListener(new NavitationLayout.OnNaPageChangeListener() {
+        navitationScrollLayout.setOnNaPageChangeListener(new NavitationScrollLayout.OnNaPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
